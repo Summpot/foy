b440e9dd63c208ec51bb2dc38c63607b7480dec7
Summpot <summpot@outlook.com>
2025-04-04 22:56:48 +0800

    Add biome

--- rslib.config.ts
+++ rslib.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from '@rslib/core'
 
 export default defineConfig({
   lib: [
-
-    { dts: true, format: 'esm', syntax: 'es2021' },
-    { dts: true, format: 'cjs', syntax: 'es2021' },
+    {
+      dts: true, bundle: false, format: 'esm', syntax: 'es2021', source: { include: ["./src/*.ts"] }
+    },
+    { dts: true, bundle: false, format: 'cjs', syntax: 'es2021', source: { include: ["./src/*.ts"] } },
   ],
 })
